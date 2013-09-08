2b4bbbba47d7696fa8c9ce25c5698da9c2886c16
leo <cwq0312@163.com>
2013-09-08 23:02:42 +0800

    大幅提升多重复杂结构的性能

--- src/main/java/org/qmik/qmikjson/out/Array2Text.java
+++ src/main/java/org/qmik/qmikjson/out/Array2Text.java
@@ -1,7 +1,6 @@
 package org.qmik.qmikjson.out;
 
 import java.io.IOException;
-import java.lang.ref.SoftReference;
 import java.text.DateFormat;
 import java.util.Collection;
 import java.util.Iterator;
@@ -9,13 +8,7 @@ import java.util.Map;
 import org.qmik.qmikjson.util.MixUtil;
 
 public class Array2Text extends Base2Text {
-	private static ThreadLocal<SoftReference<CharWriter>>	gtl_writers	= new ThreadLocal<SoftReference<CharWriter>>() {
-																								@SuppressWarnings({ "unchecked", "rawtypes" })
-																								protected SoftReference<CharWriter> initialValue() {
-																									return new SoftReference(new CharWriter(1024));
-																								};
-																							};
-	private static Array2Text										instance		= new Array2Text();
+	private static Array2Text	instance	= new Array2Text();
 	
 	private Array2Text() {
 	}
@@ -24,13 +17,8 @@ public class Array2Text extends Base2Text {
 		return instance;
 	}
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public String list2JSON(Collection<Object> list, DateFormat df) {
-		CharWriter writer = gtl_writers.get().get();
-		if (writer == null) {
-			gtl_writers.set(new SoftReference(new CharWriter(1024)));
-		}
-		writer.clear();
+		CharWriter writer = new CharWriter(getSize(list));
 		try {
 			list2JSON(writer, list, df);
 		} catch (IOException e) {
@@ -54,21 +42,27 @@ public class Array2Text extends Base2Text {
 			} else if (MixUtil.isPrimitive(value.getClass())) {
 				writer.append(String.valueOf(value));
 			} else if (value instanceof Map) {
-				Data2Text.getInstance().map2JSON(writer, (Map) value, df);
+				CharWriter cw = getCaches(value);
+				if (cw == null) {
+					cw = new CharWriter();
+					Data2Text.getInstance().map2JSON(cw, (Map) value, df);
+					setChaches(value, cw);
+				}
+				writer.append(cw);
 			} else if (value instanceof Collection) {
 				if (value == list) {
 					continue;
 				}
-				list2JSON(writer, (Collection) value, df);
+				list2JSON(writer, ((Collection) value), df);
 			} else {
-				writer.append("\"").append(value.toString()).append('"');
+				Bean2Text.getInstance().toStringWriter(writer, value, df);
 			}
 			if (vals.hasNext()) {
 				writer.append(',');
 			}
 		}
-		writer.append(']');
 		
+		writer.append(']');
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
